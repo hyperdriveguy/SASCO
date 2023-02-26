cebe1dbbec96d953ce51a5803142c47c9fd71923
Carson Bush <hyperdriveguy@gmail.com>
2023-02-26 14:48:26 -0700

    Fix player animations

--- player.gd
+++ player.gd
@@ -1,34 +1,40 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+@export var move_speed: float = 300.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var animation_state = $AnimationTree.get("parameters/playback")
 @onready var initial_postion = position
 
 func _ready():
 	pass
 
 
+# parameters/idle/blend_position
+
+
 func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction_x = Input.get_axis("ui_left", "ui_right")
-	var direction_y = Input.get_axis("ui_up", "ui_down")
-	
-	if direction_x:
-		velocity.x = direction_x * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	if direction_y:
-		velocity.y = direction_y * SPEED
-	else:
-		velocity.y = move_toward(velocity.x, 0, SPEED)
-		
+	var direction = Vector2(
+		Input.get_axis("ui_left", "ui_right"),
+		Input.get_axis("ui_up", "ui_down")
+	)
+	update_animation_blend(direction)
+	update_animation_state(direction)
+	velocity = direction * move_speed
 	if Input.is_action_just_pressed("reset_player"):
 		position = initial_postion
-
 	move_and_slide()
+
+
+func update_animation_blend(input_direction: Vector2):
+	if input_direction != Vector2.ZERO:
+		$AnimationTree.set("parameters/idle/blend_position", input_direction)
+		$AnimationTree.set("parameters/walk/blend_position", input_direction)
+
+func update_animation_state(input_direction: Vector2):
+	if input_direction == Vector2.ZERO:
+		animation_state.travel("idle")
+	else:
+		animation_state.travel("walk")
