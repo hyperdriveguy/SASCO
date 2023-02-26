1da7998deb3989c5ad96df9c7b6ba47723379fae
Carson Bush <hyperdriveguy@gmail.com>
2023-02-25 19:01:10 -0700

    Remove acceleration mode

--- player.gd
+++ player.gd
@@ -5,8 +5,6 @@ const SPEED = 300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-var accel_mode = false
-
 @onready var initial_postion = position
 
 func _ready():
@@ -20,28 +18,15 @@ func _physics_process(delta):
 	var direction_x = Input.get_axis("ui_left", "ui_right")
 	var direction_y = Input.get_axis("ui_up", "ui_down")
 	
-	if not accel_mode:
-		if direction_x:
-			velocity.x = direction_x * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-
-		if direction_y:
-			velocity.y = direction_y * SPEED
-		else:
-			velocity.y = move_toward(velocity.x, 0, SPEED)
+	if direction_x:
+		velocity.x = direction_x * SPEED
 	else:
-		if direction_x:
-			velocity.x += direction_x * SPEED / 8
-		if direction_y:
-			velocity.y += direction_y * SPEED / 8
-			
-	if Input.is_action_just_pressed("ui_accept"):
-		if accel_mode:
-			$Label.text = "velocity mode"
-		else:
-			$Label.text = "acceleration mode"
-		accel_mode = not accel_mode
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.x, 0, SPEED)
 		
 	if Input.is_action_just_pressed("reset_player"):
 		position = initial_postion
